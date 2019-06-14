078274957ac6c3bb1db4e4cca908d93fcf53eb4c
Eleazar Maestre <eleazarenrique23@gmail.com>
2019-06-14 16:24:39 -0400

    Adding basic Loopback structure project with base models

---
+++ griller-api/server/boot/automigrate.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = async function(app) {
+  let ds = app.dataSources.MongoDb;
+  let modelsArray = ['User',
+                      'AccessToken',
+                      'ACL',
+                      'RoleMapping',
+                      'Role',
+                      'Client',
+                      'CustomToken',
+                      'Griller'];
+
+    const promiseMigrate = function(model) {
+      return new Promise((resolve, reject) => {
+        ds.automigrate(model, (err) => {
+          if(err) reject(err);
+          resolve();
+        })
+      })
+    }
+
+    for(const model of modelsArray) {
+        await promiseMigrate(model);
+        console.log('Model ' + model + ' migrated to MongoDB.');
+    }
+};
