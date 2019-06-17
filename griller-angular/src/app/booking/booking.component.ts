b2a0536a055e5b3a05025b4d713bb449a0e8ebeb
Eleazar Maestre <eleazarenrique23@gmail.com>
2019-06-17 01:18:57 -0400

    Change map feature to Home

--- griller-angular/src/app/booking/booking.component.ts
+++ griller-angular/src/app/booking/booking.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +6,7 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent implements OnInit {
-  title: string = 'My first AGM project';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  
 
   constructor() { }
 
--- griller-angular/src/app/home/home.component.ts
+++ griller-angular/src/app/home/home.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
 
   constructor() { }
 
