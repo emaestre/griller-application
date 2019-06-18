59c725c93986f0e2b3a4d2addc0fb707f96044bd
Eleazar Maestre <eleazarenrique23@gmail.com>
2019-06-17 20:51:42 -0400

    Few changes in services to make API to call to get lat and lng

--- griller-angular/src/app/_services/index.ts
+++ griller-angular/src/app/_services/index.ts
@@ -2,3 +2,4 @@ export * from './authentication.service';
 export * from './signup.service';
 export * from './griller.service';
 export * from './booking.service';
+export * from './location.service';
